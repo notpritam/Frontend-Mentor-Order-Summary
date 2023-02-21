992f6680be6da9b10a1b2343c02e22df89dba3a4
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-21 11:21:21 +0530

    Challenge Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img className="hero-img" src="images/illustration-hero.svg" />
+        <div class="contain">
+          <h1>Order Summary</h1>
+          <p className="des">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+
+          <div className="row">
+            <div class="title">
+              <img src="images/icon-music.svg" />
+              <div class="price">
+                <h2>Annual Plan</h2>
+                <p>$59.99/year</p>
+              </div>
+            </div>
+
+            <a href="#">Change</a>
+          </div>
+
+          <div className="footer">
+            <button>Proceed to Payment</button>
+            <a href="">Cancel Order</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
